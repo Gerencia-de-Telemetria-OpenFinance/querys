1690cdbf8a61331ffeef2c85b381942f44966abf
Rafael Marinho dos Anjos <rafaelmarinhoanjos@gmail.com>
2025-11-25 10:31:10 -0300

    Feat: new "disponibilidade" query with CTE

--- disponibilidade/disp.sql
+++ disponibilidade/disp.sql
@@ -1,139 +1,71 @@
 -- DISPONIBILIDADE
 WITH parameters AS (
     SELECT
-        DATE('2025-03-01') as init_date
-    ,   DATE('2025-09-30') as end_date
+        DATE('2025-08-01') as init_date
+    ,   DATE('2025-10-31') as end_date
 ),
 
-base as (
+calls AS (
     SELECT
-        organisationid AS serverorgid,
-        NULL AS clientorgid,
-        -- serverorgid,
-        -- clientorgid,
-        endpoint,
-        httpmethod,
-        ts_to_date,
-        ts,
-        statuscode,
-        'CLIENT' AS role,
-        -- role,
-        status
-    FROM "pcm-product"."pcm_reports_opendata"
-    -- FROM pmc_reports_clients
+        serverorgid
+    ,   endpoint
+    ,   httpmethod
+    ,   ts_to_date_gmt as ts_to_date
+    ,   date_format(ts, '%Y-%m-%d %H:%i:00') as minute_ts
+    ,   statuscode
+    ,   COUNT(1) as request_num
+    FROM pcm_reports_clients
     WHERE 1=1
         AND (
-            statuscode = '408'
-            OR statuscode like '5%'
-            OR statuscode like '2%'
-            OR statuscode = '422'
+            role = 'CLIENT'
+            OR (role = 'SERVER' AND status = 'UNPAIRED')
         )
-        AND DATE(ts_to_date_gmt) BETWEEN
+        AND status <> 'PAIRED_INCONSISTENT'
+        AND DATE(ts_to_date_gmt) BETWEEN 
             (SELECT init_date FROM parameters)
                 AND
             (SELECT end_date FROM parameters)
+        AND (
+            statuscode LIKE '2%'
+            OR statuscode LIKE '5%'
+            OR statuscode IN ('422', '408')
+        )
+    GROUP BY 1, 2, 3, 4, 5, 6
+),
+
+sucess AS (
+    SELECT
+        serverorgid
+    ,   endpoint
+    ,   httpmethod
+    ,   ts_to_date
+    ,   minute_ts
+    ,   CAST(SUM(CASE WHEN statuscode LIKE '2%' OR statuscode = '422' THEN request_num ELSE 0 END) AS DOUBLE) as sucess
+    ,   SUM(request_num) as total_requests
+    FROM calls
+    GROUP BY 1, 2, 3, 4, 5
+),
+
+disponible AS (
+    SELECT
+        serverorgid
+    ,   endpoint
+    ,   httpmethod
+    ,   ts_to_date
+    ,   SUM(CASE WHEN sucess / total_requests >= 0.95 THEN 1 ELSE 0 END) as minutes_disponible
+    ,   COUNT(1) as total_minutes
+    ,   SUM(total_requests) as volume
+    FROM sucess
+    GROUP BY 1, 2, 3, 4
 )
 
 SELECT
-                calc_disp_dia.serverorgid,
-                calc_disp_dia.clientorgid,
-                calc_disp_dia.endpoint,
-                calc_disp_dia.httpmethod,
-                calc_disp_dia.ts_to_date,
-                calc_disp_dia.total_valid_requests_day,
-                calc_disp_dia.total_successful_requests,
-                calc_disp_dia.total_failed_requests,
-                calc_disp_dia.total_minutes_available,
-                calc_disp_dia.total_minutes_unavailable
-                ROUND(
-                    CAST(calc_disp_dia.total_minutes_available AS DOUBLE) /
-                    CASE
-                        WHEN calc_disp_dia.total_minutes_available + calc_disp_dia.total_minutes_unavailable = 0
-                        THEN NULL
-                        ELSE
-                            CAST(calc_disp_dia.total_minutes_available + calc_disp_dia.total_minutes_unavailable AS DOUBLE)
-                            END,
-                    2
-                ) AS percent_available_day
-          FROM (
-              SELECT 
-                    calc_disp_minuto.serverorgid,
-                    calc_disp_minuto.clientorgid,
-                    calc_disp_minuto.endpoint,
-                    calc_disp_minuto.httpmethod,
-                    calc_disp_minuto.ts_to_date,
-                    sum(total_chamadas_validas) as total_valid_requests_day,
-                    sum(success) as total_successful_requests,
-                    sum(errors) as total_failed_requests,
-                    SUM(
-                        CASE
-                            WHEN calc_disp_minuto.disponibilidade_no_minuto >= 95 THEN 1
-                            ELSE 0
-                        END
-                    ) AS total_minutes_available,
-                    SUM(
-                        CASE
-                            WHEN calc_disp_minuto.disponibilidade_no_minuto < 95 THEN 1
-                            ELSE 0
-                        END
-                    ) AS total_minutes_unavailable
-              FROM (
-                  SELECT
-                        filtro_minuto.serverorgid,
-                        filtro_minuto.clientorgid,
-                        filtro_minuto.endpoint,
-                        filtro_minuto.httpmethod,
-                        filtro_minuto.ts_to_date,
-                        filtro_minuto.minutes_timestamp,
-                        filtro_minuto.total_chamadas_validas,
-                        filtro_minuto.success,
-                        filtro_minuto.errors,
-                        CAST(ROUND((CAST(filtro_minuto.success AS DOUBLE) / (filtro_minuto.success + filtro_minuto.errors)) * 100, 2) AS DOUBLE) AS disponibilidade_no_minuto
-                    FROM (
-                        SELECT
-                            serverorgid,
-                            clientorgid,
-                            endpoint,
-                            httpmethod,
-                            ts_to_date,
-                            date_format(ts, '%Y-%m-%d %H:%i:00') AS minutes_timestamp,
-                            COUNT(*) AS total_chamadas_validas,
-                            SUM(
-                                CASE
-                                    WHEN (r.statuscode = '408' OR r.statuscode like '5%') THEN 1
-                                    ELSE 0
-                                END
-                            ) AS errors,
-                            SUM(
-                                CASE
-                                    WHEN r.statuscode like '2%' OR r.statuscode = '422' THEN 1
-                                    ELSE 0
-                                END
-                            ) AS success
-                        FROM base r
-                        WHERE
-                            (
-                                r.role = 'CLIENT'
-                                OR (
-                                    r.role = 'SERVER'
-                                    AND r.status = 'UNPAIRED'
-                                )
-                            )
-                        GROUP BY
-                            serverorgid,
-                            clientorgid,
-                            endpoint,
-                            httpmethod,
-                            ts_to_date,
-                            date_format(ts, '%Y-%m-%d %H:%i:00')
-                    ) AS filtro_minuto
-             ) AS calc_disp_minuto
-    GROUP by
-        calc_disp_minuto.serverorgid,
-        calc_disp_minuto.clientorgid,
-        calc_disp_minuto.endpoint,
-        calc_disp_minuto.httpmethod,
-        calc_disp_minuto.ts_to_date
-) as calc_disp_dia
-
-order by ts_to_date
+    serverorgid
+,   endpoint
+,   httpmethod
+,   ts_to_date
+,   minutes_disponible
+,   total_minutes
+,   volume
+,   CAST(minutes_disponible AS DOUBLE) / total_minutes as disp_dia
+FROM disponible
