f5c6b3bf3eb9b40f3fe23fb8fc994956789d2b84
Rafael Marinho dos Anjos <rafaelmarinhoanjos@gmail.com>
2026-01-13 09:32:35 -0300

    Update products identification logic

--- reports/pairment.sql
+++ reports/pairment.sql
@@ -1,8 +1,8 @@
 -- REPORT
 WITH parameters as (
     SELECT
-        DATE '2025-10-01' as init_date
-    ,   DATE '2025-10-31' as end_date
+        DATE '2025-11-16' as init_date
+    ,   DATE '2025-11-30' as end_date
 ),
 
 requests as (
@@ -17,13 +17,20 @@ requests as (
     ,   status
     ,   arrived_at - INTERVAL '3' HOUR AS arrived_at
     ,   ts - INTERVAL '3' HOUR AS ts
-    FROM pcm_reports_clients
+    FROM "pcm_product"."pcm_reports_clients"
     WHERE 1=1
         AND ts_to_date_gmt BETWEEN 
             (SELECT init_date FROM parameters) AND (SELECT end_date FROM parameters)
-            
+        AND (
+            orgid = clientorgid
+            OR (
+                orgid = serverorgid
+                AND status = 'UNPAIRED'
+            )
+        )
+
     UNION ALL
-    
+
     SELECT
         ts_to_date_gmt as ts_to_date
     ,   serverorgid
@@ -32,25 +39,56 @@ requests as (
     ,   endpoint
     ,   httpmethod
     ,   CASE
-            WHEN r.additionalinfo_authorisationflow = 'FIDO_FLOW' THEN 'JSR'
-            WHEN r.additionalinfo_authorisationflow = 'HYBRID_FLOW' AND r.endpoint LIKE '%pix/payments%' THEN 'PAGAMENTOS_IMEDIATOS'
-            WHEN r.additionalinfo_authorisationflow = 'HYBRID_FLOW' AND r.endpoint LIKE '%pix/recurring-payments%' AND r.additionalinfo_paymenttype = 'AUTOMATIC' THEN 'PIX_AUTOMATICO'
-            WHEN r.additionalinfo_authorisationflow = 'HYBRID_FLOW' AND r.endpoint LIKE '%pix/recurring-payments%' AND r.additionalinfo_paymenttype = 'SWEEPING' THEN 'TRANSF_INTEL'
-            ELSE 'NAO_INFORMADO'
+            WHEN (additionalinfo_authorisationflow = 'FIDO_FLOW' AND endpoint LIKE '%pix/payments%') OR endpoint LIKE '%enrollments' THEN 'JSR'
+            WHEN (endpoint LIKE '%/consents%' OR endpoint LIKE '%pix/payments%') THEN 'PAGAMENTOS_IMEDIATOS'
+            WHEN (endpoint LIKE '%recurring-consents%' OR endpoint LIKE '%pix/recurring-payments%') AND additionalinfo_paymenttype = 'AUTOMATIC' THEN 'PIX_AUTOMATICO'
+            WHEN (endpoint LIKE '%recurring-consents%' OR endpoint LIKE '%pix/recurring-payments%') AND additionalinfo_paymenttype = 'SWEEPING' THEN 'TRANSF_INTEL'
+            ELSE 'TRANSF_INTEL'
         END AS produto
     ,   status
     ,   arrived_at - INTERVAL '3' HOUR AS arrived_at
     ,   ts - INTERVAL '3' HOUR AS ts
-    FROM pcm_reports_payments
+    FROM "pcm_product"."pcm_reports_payments"
     WHERE 1=1
         AND ts_to_date_gmt BETWEEN 
             (SELECT init_date FROM parameters) AND (SELECT end_date FROM parameters)
+        AND (
+            orgid = clientorgid
+            OR (
+                orgid = serverorgid
+                AND status = 'UNPAIRED'
+            )
+        )
+        AND (
+            additionalinfo_authorisationflow = 'FIDO_FLOW'
+            OR (
+                additionalinfo_authorisationflow = 'HYBRID_FLOW'
+                AND (endpoint LIKE '%/consents%' OR endpoint LIKE '%pix/payments%' OR endpoint LIKE '%recurring-consents%' OR endpoint LIKE '%pix/recurring-payments%')
+            )
+        )
 ),
 
 pairment as (
     SELECT
         ts_to_date
-    ,   CASE WHEN role = 'CLIENT' THEN clientorgid ELSE serverorgid END as orgid
+    ,   clientorgid as orgid
+    ,   endpoint
+    ,   httpmethod
+    ,   produto
+    ,   CASE WHEN arrived_at <= (CAST(ts_to_date AS TIMESTAMP) + INTERVAL '1' DAY + INTERVAL '8' HOUR) THEN 1 ELSE 0 END AS eight_hour
+    ,   CASE WHEN arrived_at <= (ts + INTERVAL '7' DAY) THEN 1 ELSE 0 END AS seven_days
+    ,   SUM(CASE WHEN status = 'PAIRED' THEN 1 ELSE 0 END) as paired_count
+    ,   SUM(CASE WHEN status = 'UNPAIRED' THEN 1 ELSE 0 END) as unpaired_self_count
+    ,   SUM(CASE WHEN status = 'PAIRED_INCONSISTENT' THEN 1 ELSE 0 END) as inconsisten_count
+    ,   SUM(CASE WHEN status = 'SINGLE' THEN 1 ELSE 0 END) as single_count
+    FROM requests
+    GROUP BY 1, 2, 3, 4, 5, 6, 7
+
+    UNION ALL
+
+    SELECT
+        ts_to_date
+    ,   serverorgid as orgid
     ,   endpoint
     ,   httpmethod
     ,   produto
